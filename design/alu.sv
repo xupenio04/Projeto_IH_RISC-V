9e0b33257b13cd3322531b0573705dad744eb2f5
xupenio04 <106712324+xupenio04@users.noreply.github.com>
2023-08-10 09:08:02 -0300

    Update alu.sv

--- design/alu.sv
+++ design/alu.sv
@@ -17,13 +17,19 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+            4'b0001:        // OR
+                    ALUResult = SrcA | SrcB;
+           
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
+            4'b0101:          //SUB
+                    ALUResult = SrcA - SrcB;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+            4'b1001:
+                   ALUResult = SrcA ^ SrcB;
             default:
                     ALUResult = 0;
             endcase
         end
 endmodule
-
